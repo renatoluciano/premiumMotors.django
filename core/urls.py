b3737dadac010c82d088d4816f5f587134a62e40
Rento Dutra Luciano <renatoluciano@gmail.com>
2026-07-14 12:49:59 -0300

    Passo 10: Arquivo core/urls.py completo com rotas de midia e catalogo

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf import settings        
-from django.conf.urls.static import static  
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    # Painel Administrativo nativo do Django
     path('admin/', admin.site.urls),
+    
+    # Rota principal da loja (Conecta diretamente ao app catalogo)
+    path('', include('catalogo.urls')),
 ]
 
-# Libera o acesso às fotos dos carros durante o desenvolvimento local
+# Libera o acesso às fotos dos carros enviados pelo painel durante o desenvolvimento local
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
